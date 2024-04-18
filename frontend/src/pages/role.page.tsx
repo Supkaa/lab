749c1407a4c767df505ec063a9e75175c0cad444
v.b.kolpakov <v.b.kolpakov@tmn3.etagi.com>
2024-04-18 15:41:20 +0500

    add: endpoint table

--- frontend/src/pages/role.page.tsx
+++ frontend/src/pages/role.page.tsx
@@ -1,19 +1,17 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
+import {IRole} from "../interfaces/IRole";
 
 const RolePage = () => {
     const [roleId, setRoleId] = useState<string>('');
     const [endpointId, setEndpointId] = useState<string>('');
-    const [ids, setIds] = useState<string>('');
-    interface IRole {
-        name: string;
-        endpoint: {
-            id: string,
-            url: string
-            method: string
-        }
+    interface IEndpoint {
+        id: string
+        url: string
+        method: string
     }
-
+    const [endpointUrl, setEndpointUrl] = useState<IEndpoint[]>([]);
+    const [ids, setIds] = useState<string>('');
     const [roles, setRoles] = useState<IRole[]>([]);
     let token:null | string = null;
     const userData = localStorage.getItem('user');
@@ -27,6 +25,9 @@ const RolePage = () => {
     useEffect(() => {
         axios.get('http://localhost:3333/roles', {headers: {Authorization: `Bearer ${token}`}}).then((response) => {
             setRoles(response.data);
+        })
+        axios.get('http://localhost:3333/endpoints', {headers: {Authorization: `Bearer ${token}`}}).then((response) => {
+            setEndpointUrl(response.data);
         })
     }, []);
 
@@ -97,20 +98,20 @@ const RolePage = () => {
                 )}
             </div>
             <div className={'flex gap-7 mt-4 w-1/2'}>
-            <div className={'flex flex-col w-full gap-4'}>
-                <input
-                    type="text"
-                    onChange={(event) => setIds(event.target.value)}
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Название роли"
-                />
-                <input
-                    type="text"
-                    onChange={(event) => setEndpointId(event.target.value)}
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="id эндпоинта"
-                />
-            </div>
+                <div className={'flex flex-col w-full gap-4'}>
+                    <input
+                        type="text"
+                        onChange={(event) => setIds(event.target.value)}
+                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Название роли"
+                    />
+                    <input
+                        type="text"
+                        onChange={(event) => setEndpointId(event.target.value)}
+                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="id эндпоинта"
+                    />
+                </div>
                 <button
                     onClick={() => {
                         axios.post(`http://localhost:3333/roles`, {
@@ -125,6 +126,34 @@ const RolePage = () => {
                     Создать роль
                 </button>
             </div>
+            <table className="w-1/2 text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 mt-4">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th className="px-6 py-3">
+                        Id
+                    </th>
+                    <th  className="px-6 py-3">
+                        Метод
+                    </th>
+
+                </tr>
+                </thead>
+                <tbody>
+                {endpointUrl?.map((endpoint) => (
+                    <tr key={endpoint.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {endpoint.id}
+                        </td>
+                        <td className="px-6 py-4 flex gap-4">
+                            {endpoint.url}
+                        </td>
+                        <td className="px-6 py-4 flex gap-4">
+                            {endpoint.method}
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
         </div>
     )
         ;
